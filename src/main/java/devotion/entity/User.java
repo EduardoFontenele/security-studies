abc51ede8d7765ba1cd8abb52832959092b35d1b
Eduardo Fontenele <eduardofontedev@gmail.com>
2024-02-16 02:57:14 -0300

    working on it

--- src/main/java/devotion/entity/User.java
+++ src/main/java/devotion/entity/User.java
@@ -1,6 +1,5 @@
 package devotion.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
