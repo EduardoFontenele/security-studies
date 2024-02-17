eab6aba1cb2b3f18c56c80b9b5ea509a8267fad7
Eduardo Fontenele <eduardofontedev@gmail.com>
2024-02-16 23:17:32 -0300

    lets start

--- src/main/java/devotion/controller/DemoController.java
+++ src/main/java/devotion/controller/DemoController.java
@@ -1,26 +1,16 @@
 package devotion.controller;
 
-import devotion.entity.User;
-import devotion.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 public class DemoController {
-
-    private final UserRepository userRepository;
 
     @GetMapping("/demo")
     public String hello() {
         return "Hello, I'm a demo!";
     }
 
-    @GetMapping("/users")
-    public List<User> getUsers() {
-        return userRepository.findAll();
-    }
 }
