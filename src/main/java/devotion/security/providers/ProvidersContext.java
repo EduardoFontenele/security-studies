05e171fa9f15f5eaaa6e1abec9413d557db597b6
Eduardo Fontenele <eduardofontedev@gmail.com>
2024-02-21 19:22:18 -0300

    change to ProvidersContext.java

--- src/main/java/devotion/security/providers/ProvidersContext.java
+++ src/main/java/devotion/security/providers/ProvidersContext.java
@@ -1,13 +1,32 @@
 package devotion.security.providers;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-@RequiredArgsConstructor
 public class ProvidersContext {
 
     private final UsernamePwdAuthenticationProvider usernamePwdAuthenticationProvider;
 
+    @Getter
+    private final List<AuthenticationProvider> providers = new ArrayList<>();
 
+    public ProvidersContext(UsernamePwdAuthenticationProvider usernamePwdAuthenticationProvider) {
+        this.usernamePwdAuthenticationProvider = usernamePwdAuthenticationProvider;
+
+        providers.add(usernamePwdAuthenticationProvider);
+    }
+
+
+    public void addProvider(AuthenticationProvider provider) {
+        providers.add(provider);
+    }
+
+    public void resetProvidersToDefault() {
+        providers.clear();
+    }
 }
