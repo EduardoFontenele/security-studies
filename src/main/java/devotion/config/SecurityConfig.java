ad7ecad715d97897c5b9c10c1e0fa6392f723ca6
Eduardo Fontenele <eduardofontedev@gmail.com>
2024-02-22 18:14:43 -0300

    done

---
+++ src/main/java/devotion/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package devotion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsService() {
+        UserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin")
+                .authorities("write")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("user")
+                .authorities("read")
+                .build();
+
+        inMemoryUserDetailsManager.createUser(admin);
+        inMemoryUserDetailsManager.createUser(user);
+
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.GET, "/demo/1").permitAll();
+                    req.requestMatchers(HttpMethod.GET, "/demo/2").hasAuthority("write");
+                    req.requestMatchers(HttpMethod.GET, "/demo/3").hasAuthority("read");
+                    req.requestMatchers(HttpMethod.GET, "/demo/4").hasAnyAuthority("write", "read");
+                    req.anyRequest().denyAll();
+                })
+                .build();
+    }
+}
---
+++ src/main/java/devotion/controller/DemoController.java
@@ -0,0 +1,31 @@
+package devotion.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/1")
+    public String demo1() {
+        return "demo 1";
+    }
+
+    @GetMapping("/2")
+    public String demo2() {
+        return "demo 2";
+    }
+
+    @GetMapping("/3")
+    public String demo3() {
+        return "demo 3";
+    }
+
+    @GetMapping("/4")
+    public String demo4() {
+        return "demo 4";
+    }
+}
