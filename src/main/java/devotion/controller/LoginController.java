d1e38593759b5655263c56a2da7b996ccb0157a3
Eduardo Fontenele <eduardofontedev@gmail.com>
2024-02-23 14:56:49 -0300

    done

--- src/main/java/devotion/controller/LoginController.java
+++ src/main/java/devotion/controller/LoginController.java
@@ -1,16 +1,9 @@
 package devotion.controller;
 
-import devotion.dto.UserLoginDto;
 import devotion.security.CustomProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,7 +14,5 @@ public class LoginController {
 
     @PostMapping("/login")
     @PreAuthorize("permitAll()")
-    public void login(@RequestBody @Validated UserLoginDto loginDto) {
-
-    }
+    public void login() {}
 }
